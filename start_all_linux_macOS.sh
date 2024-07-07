f5d8727e0d5f26d5d40aca2e7d1150b3ad3eada4
imke-schwenke <ischwenk@smail.uni-koeln.de>
2024-07-07 18:30:33 +0200

    fixed mac bash script

--- start_all_linux_macOS.sh
+++ start_all_linux_macOS.sh
@@ -2,20 +2,25 @@
 
 # This script starts all services for the application
 # Use the following commands in your shell script
-# Make the script executable: "chmod +x start_all.sh"
+# Change the variable APP_DIR to the path where you NeuralCheck App lies
+# Change the variable CONDA_ENV_NAME to the name of your python environment
+# Make the script executable: "chmod +x start_all_linux_macOS.sh"
 # Run the script: "./start_all.sh"
 
+APP_DIR="~/Programmieren/Uni/Master/NeuralCheck"
+CONDA_ENV_NAME="tensi"
+
 # Start the UI_NeuralCheck service
-gnome-terminal -- bash -c "cd UI_NeuralCheck && npm run dev; exec bash"
+osascript -e "tell application \"Terminal\" to do script \"cd $APP_DIR/UI_NeuralCheck && npm run dev\""
 
 # Start the authentifizierungs-service
-gnome-terminal -- bash -c "cd authentifizierungs-service && python app/auth.py; exec bash"
+osascript -e "tell application \"Terminal\" to do script \"source activate $CONDA_ENV_NAME && cd $APP_DIR/authentifizierungs-service && python app/auth.py\""
 
 # Start the trainingsorchestrator-service
-gnome-terminal -- bash -c "cd trainingsorchestrator-service && python app/orchestrator.py; exec bash"
+osascript -e "tell application \"Terminal\" to do script \"source activate $CONDA_ENV_NAME && cd $APP_DIR/trainingsorchestrator-service && python app/orchestrator.py\""
 
 # Start the data-service
-gnome-terminal -- bash -c "cd data-service && python app/app_data.py; exec bash"
+osascript -e "tell application \"Terminal\" to do script \"source activate $CONDA_ENV_NAME && cd $APP_DIR/data-service && python app/app_data.py\""
 
 # Start the Trainingsservice
-gnome-terminal -- bash -c "cd Trainingsservice && python app.py; exec bash"
+osascript -e "tell application \"Terminal\" to do script \"source activate $CONDA_ENV_NAME && cd $APP_DIR/Trainingsservice && python app.py\""
